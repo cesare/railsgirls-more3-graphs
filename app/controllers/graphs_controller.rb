3f00bc917f8ae208205baeed9037a243b57b4033
SAWADA Tadashi <cesare@mayverse.jp>
2013-01-20 15:46:29 +0900

    Enable to draw graphs with parameters defined in the controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,5 +1,7 @@
 class GraphsController < ApplicationController
   def index
+    @numbers = [55, 20, 13, 32, 5, 1, 2, 10]
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @graphs }
